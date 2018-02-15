f50b07ff65530b9583dd0c6660f10b5dbf50a925
Kyle Cornwall <kyle.cornwall@healthcatalyst.com>
2018-02-15 09:56:36 -0700

    cc commit

--- src/ducks/counter.js
+++ src/ducks/counter.js
@@ -1,2 +1,67 @@
 // change this to true to see test results on the black diamond section.
-export const BLACK_DIAMOND = false;
+export const BLACK_DIAMOND = true;
+
+const INCREMENT = 'INCREMENT'
+const DECREMENT = 'DECREMENT'
+const UNDO = 'UNDO'
+const REDO = 'REDO'
+
+
+const initialState = {
+    currentValue: 0,
+    previousValues: [],
+    futureValues: []
+}
+
+export default function counter(state = initialState,action) {
+    switch(action.type) {
+        case INCREMENT:
+            return Object.assign({},state,{
+                currentValue: state.currentValue + action.amount, 
+                previousValues: [state.currentValue, ...state.previousValues],
+                futureValues: []
+            })
+        case DECREMENT: 
+            return Object.assign({},state,{
+                currentValue: state.currentValue - action.amount, 
+                previousValues: [state.currentValue, ...state.previousValues],
+                futureValues: []
+            })
+        case UNDO:
+            return Object.assign({},state,{
+                currentValue: state.previousValues[0], 
+                previousValues: state.previousValues.slice(1,state.previousValues.length), 
+                futureValues: [state.currentValue, ...state.futureValues]
+            })
+        case REDO:
+            return Object.assign({},state,{
+                currentValue: state.futureValues[0], 
+                previousValues: [state.currentValue, ...state.previousValues], 
+                futureValues:state.futureValues.slice(1,state.futureValues.length)
+            })
+        default:
+            return state;
+    }
+}
+
+export function undo() {
+    return {type: UNDO}
+}
+
+export function redo() {
+    return {type: REDO}
+}
+
+export function increment(amount)  {
+    return {
+        amount: amount,
+        type: INCREMENT
+    };
+}
+
+export function decrement(amount) {
+    return {
+        amount: amount,
+        type: DECREMENT
+    };
+}
---
+++ src/store.js
@@ -0,0 +1,5 @@
+import {createStore} from 'redux';
+
+import counter from './ducks/counter.js';
+
+export default createStore(counter);
